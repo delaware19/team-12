53aea310118645bcddfebf89ff74344f36c8f128
jcwise98 <jcwise@udel.edu>
2019-09-28 10:26:16 -0400

    slideshow

--- public/storyshow.js
+++ public/storyshow.js
@@ -41,7 +41,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "block";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
